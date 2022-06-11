4fc82f45aae8f891a3385eea3d0280cefeb9de3a
Austin Brown <austinsbrown@gmail.com>
2022-06-10 22:09:24 -0500

    got barebones display working

--- Adapta/install_process.sh
+++ Adapta/install_process.sh
@@ -1,22 +1,40 @@
+# perform updates
+sudo pacman -Syu
+
 # install yay
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ..
 
-#install display stuff
+mkdir ~/.config
+
+# install display stuff
+sudo pacman -S sddm
+sudo systemctl enable sddm.service
+
+
+# bspwm
+sudo pacman -S bspwm sxhkd
+
+cp -r bspwm ~/.config/
+chmod +x ~/.config/bspwm/bspwmrc # make it executable
+
+cp -r sxhkd ~/.config/
+chmod +x ~/.config/sxhkd/sxhkdrc # make it executable
+
+
+
+#install nitrogen
+sudo pacman -S nitrogen
+sudo cp -r backgrounds/* /usr/share
+
+sudo pacman -S alacritty
+
 
 #install zsh and oh-my-zsh
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-
-
-
-
-
-
-
 
 
