ee442c82c9b8b78e95a9d997b9c0871748746116
Austin Brown <austinsbrown@gmail.com>
2022-07-12 21:24:34 +0000

    fixed several config files

---
+++ Adapta/config.fish
@@ -0,0 +1,10 @@
+# ~/.config/fish/config.fish
+
+starship init fish | source
+
+#aliases
+alias ll="ls -A"
+alias logg="git log --decorate --all --graph --oneline"
+alias gs="git status"
+alias gf="git fetch"
+alias gc="git checkout"
